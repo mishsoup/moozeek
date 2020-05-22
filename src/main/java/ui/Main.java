3de3e2dea8f06f74a87a4d1c6cf6d0484121f725
Michelle Lei <theoriginalmish@gmail.com>
2020-05-22 01:06:22 -0700

    added random test to main

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -10,27 +10,23 @@ public class Main {
         //        p.play("I[Piano] Aq Bq Cq Dq Eq Fq Gq");
 
         // TESTS to be removed below
-        Pattern musicPattern = new Pattern("TIME:4/4");
-        musicPattern.add("Cq Dq Eq Fq Gq Aq Bq");
-        musicPattern.setInstrument("piano");
-        musicPattern.setTempo(180);
-        musicPattern.setVoice(0);
-
         Pattern musicPattern2 = new Pattern("TIME:4/4");
-        musicPattern2.add("Cq Dq Eq Fq Gq Aq Bq");
-        musicPattern2.setInstrument("violin");
-        musicPattern2.setTempo(180);
-        musicPattern2.setVoice(1);
+        musicPattern2.add("E5s D#5s E5s D#5s E5s B4s D5s C5s A4q c4s e4s a4s b4q e4s a4s b4s c5q");
+        musicPattern2.setInstrument("piano");
+        musicPattern2.setTempo(70);
+        musicPattern2.setVoice(0);
 
-        Pattern musicPattern4 = new Pattern("TIME:4/4");
-        musicPattern4.add("Cq Dq Eq Fq Gq Aq Bq");
-        musicPattern4.setInstrument("guitar");
-        musicPattern4.setTempo(300);
+        Pattern musicPattern = new Pattern("TIME:4/4");
+        musicPattern.add("Ri Rw A3s E3s A3s");
+        musicPattern.setInstrument("piano");
+        musicPattern.setTempo(70);
+        musicPattern.setVoice(1);
 
         Pattern musicPattern3 = new Pattern();
         musicPattern3.add(musicPattern2);
         musicPattern3.add(musicPattern);
-        musicPattern3.add(musicPattern4);
+
+        musicPattern3.repeat(2);
 
         p.play(musicPattern3);
 
