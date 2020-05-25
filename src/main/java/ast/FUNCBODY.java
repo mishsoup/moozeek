d5c857026a87adec5be81bd0a1118e74249dbe55
KevinXu17 <xuzhaozhishu@gmail.com>
2020-05-25 10:55:36 -0700

    1) Update parser with procedures; 2) update literals with procedures' keyword 3) Sound/FUNCBODY implements EXP interface, so the MAP will can have SOUND and FUNCBODY

---
+++ src/main/java/ast/FUNCBODY.java
@@ -0,0 +1,60 @@
+package ast;
+
+import libs.Node;
+import visitors.Visitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FUNCBODY extends Node implements EXP {
+    List<String> paraNames = new ArrayList<>();
+    List<INSTRUCTION> instructions = new ArrayList<>();
+
+    public List<String> getParaNames() {
+        return paraNames;
+    }
+
+    public List<INSTRUCTION> getInstructions() {
+        return instructions;
+    }
+
+    @Override
+    public void parse() {
+        if (tokenizer.checkToken("\\(")) {
+            tokenizer.getAndCheckNext("\\(");
+            String name1 = tokenizer.getNext();
+            paraNames.add(name1);
+            while (tokenizer.checkToken("\\,")) {
+                tokenizer.getAndCheckNext("\\,");
+                String name2 = tokenizer.getNext();
+                paraNames.add(name2);
+            }
+            tokenizer.getAndCheckNext("\\)");
+        }
+        tokenizer.getAndCheckNext("\\{");
+        while (!tokenizer.checkToken("\\}")) {
+            INSTRUCTION instruction = null;
+            if (tokenizer.checkToken("CONNECT")) {
+                instruction = new CONNECT();
+            } else if (tokenizer.checkToken("CREATE")) {
+                instruction = new CREATE();
+            } else if (tokenizer.checkToken("&")) {
+                instruction = new COMMENT();
+            } else if (tokenizer.checkToken("LAYER")) {
+                instruction = new LAYER();
+            } else if (tokenizer.checkToken("RUN")) {
+                instruction = new RUN();
+            } else {
+                throw new RuntimeException("Unknown instruction in FUNCBODY: " + tokenizer.getNext());
+            }
+            instruction.parse();
+            instructions.add(instruction);
+        }
+        tokenizer.getAndCheckNext("\\}");
+    }
+
+    @Override
+    public <T> T accept(Visitor<T> visitor) {
+        return null;
+    }
+}
---
+++ src/main/java/ast/RUN.java
@@ -0,0 +1,40 @@
+package ast;
+
+import visitors.Visitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RUN extends INSTRUCTION {
+    String funcName;
+    List<String> paraNames = new ArrayList<>();
+
+    public String getFuncName() {
+        return funcName;
+    }
+
+    public List<String> getParaNames() {
+        return paraNames;
+    }
+
+    @Override
+    public void parse() {
+        tokenizer.getAndCheckNext("RUN");
+        funcName = tokenizer.getNext();
+        if (tokenizer.checkToken("\\(")) {
+            String name1 = tokenizer.getNext();
+            paraNames.add(name1);
+            while (tokenizer.checkToken("\\,")) {
+                tokenizer.getAndCheckNext("\\,");
+                String name2 = tokenizer.getNext();
+                paraNames.add(name2);
+            }
+            tokenizer.getAndCheckNext("\\)");
+        }
+    }
+
+    @Override
+    public <T> T accept(Visitor<T> visitor) {
+        return null;
+    }
+}
